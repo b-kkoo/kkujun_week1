99a29d148feba251003a20b68e55caaac8faa130
b-kkoo <bmcow331@naver.com>
2024-10-18 14:36:51 +0900

    [Update] Update Rocket.cs, RocketEnergySystem.cs
    Q3_requirement 0

--- kkujun_week1/Assets/Scripts/Rocket.cs
+++ kkujun_week1/Assets/Scripts/Rocket.cs
@@ -4,42 +4,48 @@ using UnityEngine.UI;
 public class Rocket : MonoBehaviour
 {
     private Rigidbody2D _rb2d;
-    private float fuel = 100f;
-    private float maxFuel;
+    //private float fuel = 100f;
+    //private float maxFuel;
     
     private readonly float SPEED = 5f;
     private readonly float FUELPERSHOOT = 10f;
 
-    public Image fuelBar;
+    //public Image fuelBar;
+
+    RocketEnergySystem _energySystem;
     
     void Awake()
     {
         // TODO : Rigidbody2D 컴포넌트를 가져옴(캐싱)
         _rb2d = GetComponent<Rigidbody2D>();
+        _energySystem = GetComponent<RocketEnergySystem>();
 
-        maxFuel = fuel;
+        //maxFuel = fuel;
+        _energySystem.maxFuel = _energySystem.fuel;
     }
 
     private void Update()
     {
-        fuelBar.fillAmount = fuel / maxFuel;
+        //fuelBar.fillAmount = fuel / maxFuel;
+        _energySystem.SetFuelBar();
 
-        fuel += 0.1f;
-        if ( fuel >= 100f )
-        {
-            fuel = 100f;
-        }
+        //fuel += 0.1f;
+        //if ( fuel >= 100f )
+        //{
+        //    fuel = 100f;
+        //}
+        _energySystem.RestoreFuel();
     }
 
     public void Shoot()
     {
         // TODO : fuel이 넉넉하면 윗 방향으로 SPEED만큼의 힘으로 점프, 모자라면 무시
-        if (fuel >= 10f)
+        if ( _energySystem.fuel >= 10f)
         {
             Vector2 jump = new Vector2(0, SPEED);
             _rb2d.AddForce(jump, ForceMode2D.Impulse);
 
-            fuel -= FUELPERSHOOT;
+            _energySystem.fuel -= FUELPERSHOOT;
         }
     }
 }
--- kkujun_week1/Assets/Scripts/RocketEnergySystem.cs
+++ kkujun_week1/Assets/Scripts/RocketEnergySystem.cs
@@ -1,6 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class RocketEnergySystem : MonoBehaviour
 {
+    public float fuel = 100f;
+    public float maxFuel;
 
+    public Image fuelBar;
+
+    public void SetFuelBar()
+    {
+        fuelBar.fillAmount = fuel / maxFuel;
+    }
+
+    public void RestoreFuel()
+    {
+        fuel += 0.1f;
+        if (fuel >= 100f)
+        {
+            fuel = 100f;
+        }
+    }
 }
